78817499fce8715f39ddc81f7b7384a2df306ad6
Johan Kjölhede <kjolhede@telia.com>
2016-03-09 01:57:01 +0100

    Add support for configuration files

--- src/main/scala/se/gigurra/dcs/remote/Configuration.scala
+++ src/main/scala/se/gigurra/dcs/remote/Configuration.scala
@@ -1,7 +1,13 @@
 package se.gigurra.dcs.remote
 
+import java.io.FileNotFoundException
+
 import se.gigurra.heisenberg.MapData.SourceData
 import se.gigurra.heisenberg.{Schema, Parsed}
+import se.gigurra.serviceutils.json.JSON
+import se.gigurra.serviceutils.twitter.logging.Logging
+
+import scala.util.{Failure, Success, Try}
 
 case class LuaEnvironmentMap(source: SourceData = Map.empty)
   extends Parsed[LuaEnvironmentMap.type] {
@@ -21,13 +27,20 @@ case class Configuration(source: SourceData = Map.empty)
   val mappings      = parse(schema.mappings)
 }
 
-object Configuration extends Schema[Configuration] {
+object Configuration extends Schema[Configuration] with Logging {
   val rest_port     = required[Int]("rest_port", default = 12340)
   val cache_size_mb = required[Int]("cache_size_mb", default = 50)
   val mappings      = required[Seq[LuaEnvironmentMap]]("mappings", default = Seq(LuaEnvironmentMap()))
 
-  def readFromFile(): Configuration = {
-    // TODO: implement file read
-    Configuration()
+  def readFromFile(s: String = "dcs-remote-cfg.json"): Configuration = {
+    Try(JSON.read[Configuration](scala.io.Source.fromFile(s).mkString)) match {
+      case Success(cfg) => cfg
+      case Failure(e: FileNotFoundException) =>
+        logger.info(s"No config file found (path=${s}) - using default configuration")
+        Configuration()
+      case Failure(e) =>
+        logger.fatal(e, s"Failed to read config file ($e) '$s'")
+        throw e
+    }
   }
 }
